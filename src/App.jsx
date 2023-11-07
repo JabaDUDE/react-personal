5808f8ff6f6e9308f2edbc90a2b12803ee529d28
Dakota <dakotawkeast@gmail.com>
2023-11-06 19:00:06 -0500

    start resizing cards and got somewhere with style

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,15 @@ function App() {
   const FaveGames = Games.map((game, index) => {
     return <Card name={game.name} image={game.image} key={index} />
   })
-
+//mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white
   return(
     <>
-    <h1>Favorite Books I've Read</h1>
-    <div className="flex flex-wrap gap-4">
+    <h1 className="tracking-tight font-black md:text-5xl lg:text-6xl my-9">Favorite Books I've Read</h1>
+    <div className="flex gap-4">
     {FaveBooks}
     </div>
-    <h1>My Favorite Games</h1>
-    <div>{FaveGames}</div>
+    <h1 className="tracking-tight font-black md:text-5xl lg:text-6xl my-9">My Favorite Games</h1>
+    <div className="flex gap-4">{FaveGames}</div>
     </>
     
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,9 @@
 export default function Card(props){
     return(
-        <div className="flex flex-initial flex-col flex-wrap w-175">
+        <div className="flex flex-initial flex-col flex-wrap max-w-sm">
             <img 
             src={`./assets/${props.image}`} 
-            className="rounded-lg max-w-lg h-auto"    
+            className="rounded-lg w-full h-auto"    
             />
         
             <h1>{props.name}</h1>
