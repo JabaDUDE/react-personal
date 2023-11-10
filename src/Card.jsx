b5ab851c3f3c307d0a4b115967a4c101b99633ef
Dakota <dakotawkeast@gmail.com>
2023-11-10 04:22:58 -0500

    card image sizes fixed

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,9 @@
 export default function Card(props){
     return(
-        <div className="flex flex-initial flex-col flex-wrap w-100 border-red border-2 bg-blue-900 text-white text-center rounded-lg hover:border-black hover:scale-105">
+        <div className="flex flex-initial flex-col flex-wrap w-69 border-red border-2 bg-blue-900 text-white text-center rounded-lg hover:border-black hover:scale-105">
             <img 
             src={`./assets/${props.image}`} 
-            className="object-contain h-48 w-full"    
+            className="object-fill h-full w-56"    
             />
         </div>
     )
