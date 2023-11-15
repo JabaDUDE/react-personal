5f97fb3ab8ba228d017769c23c17c612553e8f2a
Dakota <dakotawkeast@gmail.com>
2023-11-15 18:42:58 -0500

    got card effect to work with custom class

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,17 +1,16 @@
 export default function Card(props){
     //TODO: Change all images to be background
     return(
-        <div className="flex flex-wrap w-69 overflow-hidden hover:text-white rounded-lg hover:scale-105 hover:brightness-[.25] relative">
+        <div className="flex flex-wrap w-69 overflow-hidden hover:text-white rounded-lg hover:scale-105 relative card--details">
         <a href={props.url} target="_blank">
             <img 
             src={`./assets/${props.image}`} 
             className="h-full w-56 hover:cursor-pointer hover:scale-110"    
             />
-            <div className="absolute hover:text-white hover:brightness-100 text-center ">
+        </a>
+        <div className="absolute text-center">
              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ultrices quis lacus ut rhoncus. Nam sit amet mauris sem. Maecenas ut pellentesque metus. Sed tristique dictum      augue, in laoreet elit venenatis id. Aliquam erat volutpat. Integer porttitor laoreet suscipit.</p>
         </div>
-        </a>
- 
         </div>
     )
 }
