090eb9383fd9e7722f6ca60c8489c5109205b4d9
Dakota <dakotawkeast@gmail.com>
2023-11-21 20:09:57 -0500

    fixed bug with font sizes

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@
 export default function Card(props){
   
     return(
-        <div className="flex flex-wrap h-54 sm:w-56 w-screen overflow-hidden hover:text-white rounded-lg hover:scale-105 relative group my-2">
+        <div className="flex flex-wrap h-54 sm:w-56 w-screen overflow-hidden hover:text-white hover:px-4 rounded-lg hover:scale-105 relative group my-2">
         
             <img 
             src={`./assets/${props.image}`} 
@@ -10,7 +10,7 @@ export default function Card(props){
             alt={props.name}   
             />
         
-        <div className="absolute hover:text-white text-center p-2 opacity-0 group-hover:opacity-100 group-hover:brightness-100 group-hover:text-white group-hover:transition-all group-hover:ease-in-out duration-500 mt-1">
+        <div className="absolute text-center opacity-0 p-3 md:text-sm group-hover:opacity-100 group-hover:brightness-100 group-hover:text-white group-hover:transition-all group-hover:ease-in-out duration-500 mt-1">
              <p>{props.summary}</p>
              <a href={props.url} target="_blank" rel="noreferrer" className="text-blue-500 underline">Learn More...</a>
         </div>
