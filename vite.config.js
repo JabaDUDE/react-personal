0b7e103366b94fe696f9e3681a99a8228130e638
Dakota <dakotawkeast@gmail.com>
2023-11-23 18:11:12 -0500

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ESLint } from 'eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/react-personal/',
-  plugins: [react()],
+  plugins: [react(), ESLint()],
 })
